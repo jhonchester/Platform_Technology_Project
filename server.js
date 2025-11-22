9c4db9dbbb568488d146f0c83cb001e093c1e2fb
jhonchester <153726909+jhonchester@users.noreply.github.com>
2025-11-22 05:29:43 +0000

    add \n for new line

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.write("Jhon Chester M. Guijoba");
-  res.write("BSIT-BA-4101");
+  res.write("Jhon Chester M. Guijoba\n");
+  res.write("BSIT-BA-4101\n");
   res.write("Believe you can and youre halfway there. Quoted By Theodore Roosevelt");
   res.end();
 });
